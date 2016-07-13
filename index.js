5f7ab19e33bffd064400477d8623d8d7fc586077
尹航 <yinhang1989@icloud.com>
2016-07-13 16:33:01 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var readline = require("readline");
 var env = process.env;
 var configFileName = "fesrcb-config.json";
 
+var readlineInterface = readline.createInterface({
+    input: childProcess.stdin,
+    output: childProcess.stdout
+});
+
 var fesrcb = {
     init: function () {
         var configFilePath = env.PWD + "/" + configFileName;
@@ -18,17 +23,16 @@ var fesrcb = {
         var tplData = fs.readFileSync(__dirname + "/res/fesrcb-config_tpl.json", "utf8");
         tplData = tplData.replace("_PROJECT_PATH_", env.PWD);
 
-        var ri = new readline.Interface();
-
-        ri.question("请输入fe-src路径: " + env.PWD + "/", function (answer) {
+        readlineInterface.question("请输入fe-src路径: " + env.PWD + "/", function (answer) {
             tplData = tplData.replace("_FESRC_PATH_", answer);
 
-            ri.question("请输入目标static路径: " + env.PWD + "/", function (answer) {
+            readlineInterface.question("请输入目标static路径: " + env.PWD + "/", function (answer) {
                 tplData = tplData.replace("_STATIC_TARGET_PATH_", answer);
 
-                ri.question("请输入目标tpl路径: " + env.PWD + "/", function (answer) {
+                readlineInterface.question("请输入目标tpl路径: " + env.PWD + "/", function (answer) {
                     tplData = tplData.replace("_TPL_TARGET_PATH_", answer);
                     fs.writeFileSync(configFilePath, tplData, "utf8");
+                    readlineInterface.close();
                 });
 
             });
